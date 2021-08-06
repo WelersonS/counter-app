9988a493fda2d9291885ad9cc55155a1c8dd24cf
welersonS <welerson58@gmail.com>
2021-08-06 14:54:46 -0300

    Finished dsv counter app /Create AppBar and StatefulWidget

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -1,5 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main () {
-  runApp( Container());
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(primarySwatch: Colors.purple),
+    home: HomePage()
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var counter = 0;
+
+  void increment() {
+    counter++;
+    setState(() {
+      
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My First App"),
+      ),
+      body: Center(
+        child: Text(
+          "COUNTER\n$counter",
+          textAlign: TextAlign.center,
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+      )
+    );
+  }
 }
